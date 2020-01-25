815842541f014c7f7e27b932e2fe0de43ff0d589
sogafish <k.sogafish@gmail.com>
2020-01-25 21:43:39 +0900

    :hammer: Fix Form1

--- src/components/Form1/index.tsx
+++ src/components/Form1/index.tsx
@@ -3,18 +3,18 @@ import { useForm, ErrorMessage } from 'react-hook-form'
 
 export default function Form1() {
   const { register, handleSubmit, watch, errors } = useForm({  validateCriteriaMode: 'all' });
-  const onSubmit = (data: Object) => { alert(JSON.stringify(data)) };
+  const onSubmit = (data: Object) => { console.table(data) };
 
   return (
     <div className="form form1">
       <h1>Form1</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
+        <div className="form-section">
           <span>1_1: </span>
           <input name="form1_1" defaultValue="test" ref={register} />
-          <span> watched</span>
+          <span className="sub-text">*watched</span>
         </div>
-        <div>
+        <div className="form-section">
           <span>1_2: </span>
           <input
             name="form1_2"
@@ -28,9 +28,11 @@ export default function Form1() {
               })
             }
           />
-          <span> required</span>
+          <span className="sub-text">*required</span>
         </div>
-        <p className="form1-watch-text">watch output: {watch('form1_1')}</p>
+        <div className="form1-watch">
+          <p className="form1-watch-text">watch output: {watch('form1_1')}</p>
+        </div>
         <div className="errors">
           <ErrorMessage errors={errors} name="form1_2" />
         </div>
